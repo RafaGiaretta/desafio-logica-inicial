517737f8768262f66ab30c36c685fa39a904d620
Rafael Giaretta <rafaelgiaretta@ymail.com>
2024-04-26 15:21:22 -0300

    Desafio feito em JS

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
-console.log("Hello World")
+console.log("Desafio Inicial");
+
+let nickUsuario = "Rafa Giaretta"
+let xpHeroi = 9001;
+
+
+
+function ranking(xp){
+    if (xp < 1000){
+        return ("Ferro")
+    }
+    if (xp >= 1000 && xp <= 2000){
+        return ("Bronze")
+    }
+    if (xp > 2000 && xp <= 5000){
+        return ("Bronze")
+    }
+    if (xp > 5000 && xp <= 6000){
+        return ("Prata")
+    }
+    if (xp > 6000 && xp <= 7000){
+        return ("Ouro")
+    }
+    if (xp > 7000 && xp <= 8000){
+        return ("Ascendente")
+    }
+    if (xp > 8000 && xp <= 9000){
+        return ("Imortal")
+    }
+    if (xp > 9000)
+        return ("Radiante")
+   
+}
+
+console.log("O Heroi de nome **"+ nickUsuario + "** esta no nivel de: **" + ranking(xpHeroi) + "**.")
